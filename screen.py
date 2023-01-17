0fa802e7ac1d29f6636ecd99adbf2b427d3e3318
Abhinav Kotamreddy <abhkot351@fusdk12.net>
2023-01-17 09:13:03 -0800

    dw

--- screen.py
+++ screen.py
@@ -1,10 +1,30 @@
-import pygame  
-pygame.init()  
-screen = pygame.display.set_mode((400,500))  
-done = False  
-  
-while not done:  
-    for event in pygame.event.get():  
-        if event.type == pygame.QUIT:  
-            done = True  
-    pygame.display.flip()  
+import pygame
+from pygame.locals import *
+
+clock = pygame.time.Clock()
+fps = 180
+
+screen_width = 600
+screen_height = 800
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Space INvaders')
+
+#load img
+bg = pygame.image.load("img/bg.jpg")
+def draw_bg():
+    screen.blit(bg, (0, 0))
+run = True
+
+while run:
+    clock.tick(fps)
+
+    #draw background
+    draw_bg()
+
+    #event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = false
+    pygame.display.update()
+pygame.quit()
